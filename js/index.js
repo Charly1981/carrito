4a5d2f57547325c3d59ce0ab43a7d31e51ba9f78
Charly <charly81.zirpoli@gmail.com>
2021-10-27 20:59:14 -0300

    Fetch del json

--- js/index.js
+++ js/index.js
@@ -4,6 +4,23 @@ document.addEventListener("DOMContentLoaded", () => {
   loadProducts();
 });
 
-function loadProducts() {
+function getProductDb() {
+  const url = "../dbProducts.json";
+
+  return fetch(url)
+    .then((response) => {
+      return response.json();
+    })
+    .then((result) => {
+      return result;
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+}
+
+async function loadProducts() {
   console.log("Funcion ejecutada OK");
+  const products = await getProductDb();
+  console.log(products);
 }
